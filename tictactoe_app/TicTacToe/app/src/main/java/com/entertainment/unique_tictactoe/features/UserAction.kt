e73c8636e72ba198ada0508aff55ace2c0e0afc7
user <monsterkil61@gmail.com>
2023-04-13 19:47:33 +0300

    Deleted unnecessary file

--- tictactoe_app/TicTacToe/app/src/main/java/com/entertainment/unique_tictactoe/features/UserAction.kt
+++
@@ -1,6 +0,0 @@
-package com.entertainment.unique_tictactoe.features
-
-sealed class UserAction {
-    object PlayAgainButtonClicked : UserAction()
-    data class BoardTapped(val cellNo : Int) : UserAction()
-}
